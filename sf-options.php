a17deed94a12821085da1da95312c0d5d0a8a124
Matt <info@mgates.me>
2012-10-10 23:30:34 -0400

    Example options added

--- sf-options.php
+++ sf-options.php
@@ -5,9 +5,17 @@ $options[] = array( 'name' => __( 'General', 'geczy' ), 'type' => 'heading' );
 $options[] = array( 'name' => __( 'General options', 'geczy' ), 'type' => 'title', 'desc' => __( '', 'geczy' ) );
 
 $options[] = array(
-	'name' => __( 'Default commission (%)', 'geczy' ),
-	'desc' => __( 'Choose the default rate for each product. If a product has a commission rate already set, this value will be ignored for that product.', 'geczy' ),
-	'id'   => 'default_commission',
+	'name' => __( 'Name', 'geczy' ),
+	'desc' => __( 'Please tell me who you are.', 'geczy' ),
+	'id'   => 'text_sample',
+	'type' => 'text',
+);
+
+$options[] = array(
+	'name' => __( 'Age', 'geczy' ),
+	'desc' => __( 'What\'s your age, buddy?.', 'geczy' ),
+	'tip'  => __('It\'s simple, just enter your age!', 'geczy'),
+	'id'   => 'number_sample',
 	'css' => 'width:70px;',
 	'type' => 'number',
 	'restrict' => array(
@@ -17,18 +25,59 @@ $options[] = array(
 );
 
 $options[] = array(
-	'name' => __( 'Orders page', 'geczy' ),
-	'desc' => __( 'Choose the page that has the shortcode <code>[pv_orders]</code>', 'geczy' ),
-	'id'   => 'orders_page',
+	'name' => __( 'Describe yourself', 'geczy' ),
+	'desc' => __( 'Which word describes you best?.', 'geczy' ),
+	'tip'  => __('If you can\'t choose, I\'ve defaulted an option for you.', 'geczy'),
+	'std'  => 'id_one',
+	'id'   => 'radio_sample',
+	'type' => 'radio',
+	'options' => array(
+		'id_one' => 'Gorgeous',
+		'id_two' => 'Pretty'
+	)
+);
+
+$options[] = array(
+	'name' => __( 'Biography', 'geczy' ),
+	'desc' => __( 'So tell me about yourself.', 'geczy' ),
+	'id'   => 'textarea_sample',
+	'type' => 'textarea',
+);
+
+$options[] = array(
+	'name' => __( 'Wordpress page', 'geczy' ),
+	'desc' => __( 'Pick your favorite page!', 'geczy' ),
+	'tip'  => __('Or maybe you don\'t have a favorite?', 'geczy'),
+	'id'   => 'single_select_page_sample',
 	'type' => 'single_select_page',
 );
 
 $options[] = array(
-	'name' => __( 'Show orders', 'geczy' ),
-	'desc' => __( 'Allow vendors to view order details for their products', 'geczy' ),
-	'id'   => 'show_orders',
+	'name' => __( 'Would you rather have', 'geczy' ),
+	'desc' => __( 'Which would you rather have?.', 'geczy' ),
+	'id'   => 'select_sample',
+	'type' => 'select',
+	'options' => array(
+		'id_one' => 'Ten dollars',
+		'id_two' => 'Five dollars',
+		'id_three' => 'Two dollars',
+		'id_four' => 'One dollar',
+	)
+);
+
+$options[] = array(
+	'name' => __( 'Terms', 'geczy' ),
+	'desc' => __( 'Agree to my terms...Or else.', 'geczy' ),
+	'id'   => 'checkbox_sample',
 	'type' => 'checkbox',
 );
 
-$options[] = array( 'name' => __( 'Advanced', 'geczy' ), 'type' => 'heading' );
-$options[] = array( 'name' => __( 'Advanced options', 'geczy' ), 'type' => 'title', 'desc' => __( 'There\'s awesome options on this page to configure!', 'geczy' ) );
+$options[] = array( 'name' => __( 'More tabs', 'geczy' ), 'type' => 'heading' );
+$options[] = array( 'name' => __( 'More tabs', 'geczy' ), 'type' => 'title', 'desc' => __( 'There\'s awesome options on this page to configure!', 'geczy' ) );
+
+$options[] = array(
+	'name' => __( 'Awesome', 'geczy' ),
+	'desc' => __( 'Is this awesome or what?', 'geczy' ),
+	'id'   => 'checkbox_sample2',
+	'type' => 'checkbox',
+);
