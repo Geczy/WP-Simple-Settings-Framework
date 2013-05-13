222e65ffe129d63966744dfbd7f41ec7fdd5db5d
Matt <info@mgates.me>
2013-05-12 23:09:54 -0400

    Make these hooks reference the class name directly

--- classes/sf-class-sanitize.php
+++ classes/sf-class-sanitize.php
@@ -21,13 +21,13 @@ if ( ! class_exists( 'SF_Sanitize' ) ) {
 		{
 			add_filter( 'geczy_sanitize_color', 'sanitize_text_field' );
 			add_filter( 'geczy_sanitize_text', 'sanitize_text_field' );
-			add_filter( 'geczy_sanitize_number', array( $this, 'sanitize_number_field' ) );
-			add_filter( 'geczy_sanitize_textarea', array( $this, 'sanitize_textarea' ) );
-			add_filter( 'geczy_sanitize_wysiwyg', array( $this, 'sanitize_wysiwyg' ) );
-			add_filter( 'geczy_sanitize_checkbox', array( $this, 'sanitize_checkbox' ), 10, 2 );
-			add_filter( 'geczy_sanitize_radio', array( $this, 'sanitize_enum' ), 10, 2 );
-			add_filter( 'geczy_sanitize_select', array( $this, 'sanitize_enum' ), 10, 2 );
-			add_filter( 'geczy_sanitize_single_select_page', array( $this, 'sanitize_select_pages' ), 10, 2 );
+			add_filter( 'geczy_sanitize_number', array( 'SF_Sanitize', 'sanitize_number_field' ) );
+			add_filter( 'geczy_sanitize_textarea', array( 'SF_Sanitize', 'sanitize_textarea' ) );
+			add_filter( 'geczy_sanitize_wysiwyg', array( 'SF_Sanitize', 'sanitize_wysiwyg' ) );
+			add_filter( 'geczy_sanitize_checkbox', array( 'SF_Sanitize', 'sanitize_checkbox' ), 10, 2 );
+			add_filter( 'geczy_sanitize_radio', array( 'SF_Sanitize', 'sanitize_enum' ), 10, 2 );
+			add_filter( 'geczy_sanitize_select', array( 'SF_Sanitize', 'sanitize_enum' ), 10, 2 );
+			add_filter( 'geczy_sanitize_single_select_page', array( 'SF_Sanitize', 'sanitize_select_pages' ), 10, 2 );
 		}
 
 
@@ -115,7 +115,7 @@ if ( ! class_exists( 'SF_Sanitize' ) ) {
 
 			if ( is_array( $input ) ) {
 				foreach ( $input as $value ) {
-					if ( !$this->sanitize_enum( $value, $option ) ) {
+					if ( !SF_Sanitize::sanitize_enum( $value, $option ) ) {
 						$output = false;
 						break;
 					}
